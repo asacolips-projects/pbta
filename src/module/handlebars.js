1489015ff409832eba25c98eaf96551936a776e0
Matheus Clemente <matheus.xb@gmail.com>
2023-11-22 22:03:18 -0300

    Visual improvements to sheets 2

--- src/module/handlebars.js
+++ src/module/handlebars.js
@@ -2,40 +2,28 @@ export class PbtaRegisterHelpers {
   static init() {
     Handlebars.registerHelper('pbtaTags', function(tagsInput) {
       const tags = JSON.parse(tagsInput);
-      let output = '<div class="tags">';
-      for (let tag of tags) {
-        output += `<div class="tag">${tag.value}</div>`;
-      }
-      output += '</div>';
+      const tagList = tags.map(tag => `<div class="tag">${tag.value}</div>`).join('');
+      const output = `<div class="tags">${tagList}</div>`;
       return output;
     });
 
-    Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-      return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    /**
+     * Similar to Foundry's eq, except "1" == 1 is truthy.
+     */
+    Handlebars.registerHelper('softEq', function(arg1, arg2, options) {
+      return (arg1 == arg2);
+    });
+
+    /**
+     * Returns length of Object's keys.
+     */
+    Handlebars.registerHelper("objLen", function(json) {
+      return Object.keys(json).length;
     });
 
     Handlebars.registerHelper('getLabel', function(obj, key) {
-      let result = key;
-      if (typeof obj == 'object' && obj[key]) {
-        result = (typeof obj[key] == 'object' && obj[key].label) ? obj[key].label : obj[key];
-      }
+      const result = obj[key]?.label || obj[key] || key;
       return result.length > 0 ? result : key;
-    });
-
-    Handlebars.registerHelper('progressCircle', function(data) {
-      return `<svg class="progress-ring progress-ring--${data.class}" viewBox="0 0 ${data.diameter} ${data.diameter}" width="${data.diameter}" height="${data.diameter}">
-      <circle
-        class="progress-ring__circle"
-        stroke-width="${data.strokeWidth}"
-        stroke-dasharray="${data.circumference}"
-        stroke-dashoffset="${data.offset}"
-        stroke="${data.color}"
-        fill="transparent"
-        r="${data.radius}"
-        cx="${data.position}"
-        cy="${data.position}"
-      />
-    </svg>`;
     });
   }
 }
