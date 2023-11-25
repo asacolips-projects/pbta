af7f8624a851420db68f89fa468f4500142c9d22
Matheus Clemente <matheus.xb@gmail.com>
2023-11-25 04:27:40 -0300

    Update combat-tracker.js

--- src/module/combat/combat-tracker.js
+++ src/module/combat/combat-tracker.js
@@ -251,4 +251,42 @@ export class PbtACombatTracker extends CombatTracker {
 			}
 		});
 	}
+
+	_getEntryContextOptions() {
+		return [
+			{
+				name: "COMBAT.CombatantUpdate",
+				icon: '<i class="fas fa-edit"></i>',
+				callback: this._onConfigureCombatant.bind(this)
+			},
+			// {
+			// 	name: "COMBAT.CombatantClear",
+			// 	icon: '<i class="fas fa-undo"></i>',
+			// 	condition: li => {
+			// 		const combatant = this.viewed.combatants.get(li.data("combatant-id"));
+			// 		return Number.isNumeric(combatant?.initiative);
+			// 	},
+			// 	callback: li => {
+			// 		const combatant = this.viewed.combatants.get(li.data("combatant-id"));
+			// 		if ( combatant ) return combatant.update({initiative: null});
+			// 	}
+			// },
+			// {
+			// 	name: "COMBAT.CombatantReroll",
+			// 	icon: '<i class="fas fa-dice-d20"></i>',
+			// 	callback: li => {
+			// 		const combatant = this.viewed.combatants.get(li.data("combatant-id"));
+			// 		if ( combatant ) return this.viewed.rollInitiative([combatant.id]);
+			// 	}
+			// },
+			{
+				name: "COMBAT.CombatantRemove",
+				icon: '<i class="fas fa-trash"></i>',
+				callback: li => {
+					const combatant = this.viewed.combatants.get(li.data("combatant-id"));
+					if ( combatant ) return combatant.delete();
+				}
+			}
+		];
+	}
 }
