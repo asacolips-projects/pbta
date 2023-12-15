bce3be6d629a0e459c3f693694337944c50f7fc1
Matheus Clemente <matheus.xb@gmail.com>
2023-12-15 16:53:33 -0300

    Actor Model: turn details into a MappingField

--- src/module/data/actor/templates/actor.js
+++ src/module/data/actor/templates/actor.js
@@ -1,12 +1,13 @@
+import { MappingField } from "../../shared.js";
+
 export class ActorDataTemplate extends foundry.abstract.DataModel {
 	static defineSchema() {
 		return {
 			stats: new foundry.data.fields.ObjectField(),
 			attrTop: new foundry.data.fields.ObjectField(),
 			attrLeft: new foundry.data.fields.ObjectField(),
-			details: new foundry.data.fields.SchemaField({
-				biography: new foundry.data.fields.HTMLField()
-			})
+			details: new MappingField(new foundry.data.fields.HTMLField(),
+				{ initialKeys: ["biography"] })
 		};
 	}
 
