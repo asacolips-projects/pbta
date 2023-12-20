84b6360a94428cafe51f4d9f3a48bb61f5828903
Matheus Clemente <matheus.xb@gmail.com>
2023-12-20 19:07:22 -0300

    Update playbook.js

--- src/module/data/item/playbook.js
+++ src/module/data/item/playbook.js
@@ -3,8 +3,19 @@ import { ItemTemplateData } from "./templates/item.js";
 export default class PlaybookData extends ItemTemplateData {
 	static defineSchema() {
 		return {
-			description: new foundry.data.fields.HTMLField(),
-			slug: new foundry.data.fields.StringField({ initial: "" })
+			description: new foundry.data.fields.HTMLField({ initial: "" }),
+			slug: new foundry.data.fields.StringField({
+				required: true,
+				validate: (value) => {
+					if (value !== value.slugify()) {
+						return new foundry.data.validation.DataModelValidationFailure({
+							unresolved: true,
+							invalidValue: value,
+							message: `${value} is not a valid slug`,
+						});
+					}
+				}
+			})
 		};
 	}
 }
