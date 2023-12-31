272e37fe98e8a7e1be139c070326b8338c3a00a8
Matheus Clemente <matheus.xb@gmail.com>
2023-12-31 01:42:46 -0300

    Update macros

--- src/module/documents/macro.js
+++ src/module/documents/macro.js
@@ -6,76 +6,50 @@
  * @returns {Promise}
  */
 export async function createPbtaMacro(data, slot) {
-	// First, determine if this is a valid owned item.
-	if (data.type !== "Item") {
-		return;
+	const macroData = { type: "script", scope: "actor" };
+	const itemData = await Item.implementation.fromDropData(data);
+	if (!itemData) {
+		ui.notifications.warn("You can only create macro buttons for owned Items");
+		return null;
 	}
-	if (!data.uuid.includes("Actor.") && !data.uuid.includes("Token.")) {
-		return ui.notifications.warn("You can only create macro buttons for owned Items");
-	}
-	// If it is, retrieve it based on the uuid.
-	const item = await Item.fromDropData(data);
-
-	// Create the macro command
-	// @todo refactor this to use uuids and folders.
-	const command = `game.pbta.rollItemMacro("${item.name}");`;
-	let macro = game.macros.find((m) => (m.name === item.name) && (m.command === command));
-	if (!macro) {
-		macro = await Macro.create({
-			name: item.name,
-			type: "script",
-			img: item.img,
-			command: command,
-			flags: {
-				"pbta.itemMacro": true,
-				"pbta.itemUuid": data.uuid
-			}
-		});
-	}
+	foundry.utils.mergeObject(macroData, {
+		name: itemData.name,
+		img: itemData.img,
+		command: `pbta.documents.macro.rollItemMacro("${itemData.name}")`,
+		flags: {
+			"pbta.itemMacro": true,
+			"pbta.itemUuid": data.uuid
+		}
+	});
+	const macro = game.macros.find((m) => {
+		return (m.name === macroData.name) && (m.command === macroData.command) && m.isAuthor;
+	}) || await Macro.create(macroData);
 	game.user.assignHotbarMacro(macro, slot);
-	return false;
 }
 
-// eslint-disable-next-line jsdoc/require-returns-check
 /**
  * Create a Macro from an Item drop.
  * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemData
- * @returns {Promise}
+ * @param {string} name
+ * @returns {}
  */
-export function rollItemMacro(itemData) {
-	// Reconstruct the drop data so that we can load the item.
-	// @todo this section isn't currently used, the name section below is used.
-	if (itemData.includes("Actor.") || itemData.includes("Token.")) {
-		const dropData = {
-			type: "Item",
-			uuid: itemData
-		};
-		Item.fromDropData(dropData).then((item) => {
-			// Determine if the item loaded and if it's an owned item.
-			if (!item || !item.parent) {
-				const itemName = item?.name ?? itemData;
-				return ui.notifications.warn(`Could not find item ${itemName}. You may need to delete and recreate this macro.`);
-			}
-
-			// Trigger the item roll
-			item.roll();
-		});
-	} else {
-		const speaker = ChatMessage.getSpeaker();
-		let actor;
-		if (speaker.token) {
-			actor = game.actors.tokens[speaker.token];
-		}
-		if (!actor) {
-			actor = game.actors.get(speaker.actor);
-		}
-		const item = actor ? actor.items.find((i) => i.name === itemData) : null;
-		if (!item) {
-			return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemData}`);
-		}
-
-		// Trigger the item roll
-		item.roll();
+export function rollItemMacro(name) {
+	let actor;
+	const speaker = ChatMessage.getSpeaker();
+	if (speaker.token) actor = game.actors.tokens[speaker.token];
+	actor ??= game.actors.get(speaker.actor);
+	if (!actor) {
+		ui.notifications.warn("PBTA.Warnings.Macro.NoActorSelected", {localize: true});
+		return null;
 	}
+
+	const documents = game.items.filter((i) => foundry.utils.getProperty(i, "name") === name);
+	if (documents.length === 0) {
+		ui.notifications.warn(game.i18n.format("PBTA.Warnings.Macro.MissingTargetWarn", { actor: actor.name, name }));
+		return null;
+	}
+	if (documents.length > 1) {
+		ui.notifications.warn(game.i18n.format("PBTA.Warnings.Macro.MultipleTargetsWarn", { actor: actor.name, name }));
+	}
+	documents[0].roll();
 }
--- src/module/pbta.js
+++ src/module/pbta.js
@@ -206,7 +206,12 @@ Hooks.once("ready", async function () {
 	}
 
 	// Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-	Hooks.on("hotbarDrop", (bar, data, slot) => documents.macro.createPbtaMacro(data, slot));
+	Hooks.on("hotbarDrop", (bar, data, slot) => {
+		if (["Item"].includes(data.type)) {
+			documents.macro.createPbtaMacro(data, slot);
+			return false;
+		}
+	});
 
 	CONFIG.PBTA = PBTA;
 
