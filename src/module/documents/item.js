302c1052358130ae1a08870229a823e6cc93bbad
Matheus Clemente <matheus.xb@gmail.com>
2024-05-27 14:31:28 -0300

    Update item.js

--- src/module/documents/item.js
+++ src/module/documents/item.js
@@ -89,7 +89,7 @@ export default class ItemPbta extends Item {
 					}
 				}
 			});
-			if (choice.options.conditionsConsumed.includes("forward")) {
+			if (r.options.conditionsConsumed.includes("forward")) {
 				await this.actor?.clearForwardAdv();
 			}
 			await this.actor.updateCombatMoveCount();
